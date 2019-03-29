50f8cc9f9e72f71936e3fb41702b443235e15c68
Brad Jones <bradster.jones@gmail.com>
2019-03-28 23:39:13 -0500

    Start converting the booster.js from my old bot into ES6, implement the list command

---
+++ classes/booster.js
@@ -0,0 +1,78 @@
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./booster.db');
+
+class Booster {
+  init() {
+    db.serialize(() => {
+        db.run("CREATE TABLE IF NOT EXISTS boosted (id TEXT PRIMARY KEY, username TEXT NOT NULL, boosted_meter INTEGER NOT NULL)");
+    });
+  }
+
+  list(callback, guild) {
+    db.serialize(() => {
+      var messages = [];
+      var memberMap = [];
+
+      if (guild && guild.members) {
+        memberMap = guild.members;
+      }
+
+      db.each("SELECT id, username, boosted_meter FROM boosted ORDER BY boosted_meter DESC", (err, row) => {
+        var displayName = row.username;
+
+        if (guild.memberCount >= 1) {
+          guild.members.forEach(guildMember => {
+            if (guildMember.id === row.id && guildMember.nickname) {
+              displayName = guildMember.nickname;
+            }
+          });
+        }
+
+        console.log(this);
+
+        messages.push("\n" + displayName + ": " + this._getBoostedRanking(row.boosted_meter));
+      }, () => {
+          let message = messages.join('\n');
+          if (callback) {
+            callback(message);
+          }
+      });
+    });
+  }
+
+  boost(boostee, callback, boostBy = 1) {
+    db.serialize(function() {
+      db.get("SELECT id, username, boosted_meter FROM boosted WHERE id = ?", [boostee.id], (err, row) => {
+        db.serialize(function() {
+          var boostedScore = boostBy;
+          if (!row) {
+            db.run("INSERT INTO boosted VALUES (?, ?, ?)", [boostee.id, boostee.username, 1]);
+          }
+          else {
+            boostedScore = row.boosted_meter + boostBy;
+          }
+
+          db.run("UPDATE boosted SET boosted_meter = ? WHERE id = ?", [boostedScore, boostee.id]);
+          
+          if (callback) {
+            callback(boostedScore);
+          }
+        });
+      });
+    });
+  }
+
+  _getBoostedRanking(boostedValue) {
+      var message = boostedValue % 100 + "%";
+
+      if (boostedValue >= 100) {
+          for (var x = boostedValue; x >= 100; x -= 100) {
+              message += " :star:";
+          }
+      }
+
+      return message;
+  }
+}
+
+module.exports = Booster;
--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const HeraldaPlugin = require('heralda-plugin-base');
+const Booster = require('./classes/booster.js');
 
 const defaultConfig = require('./config.json');
 
 class BoostedPlugin extends HeraldaPlugin {
   init(client, config) {
     this.config = HeraldaPlugin.mergeConfigs(config, defaultConfig);
-
-    this._setupDatabase();
+    this.booster = new Booster();
     this._setupCommands();
   }
 
-  _setupDatabase() {
-
+  _setupCommands() {
+    this.responder.addListener({
+      messages: this.config.commands.list,
+      privateAllowed: true,
+      callback: this._list.bind(this)
+    });
   }
 
-  _setupCommands() {
-
+  _list(message) {
+    this.booster.list(boosterMessage => {
+        message.channel.sendMessage(boosterMessage);
+    }, message.guild);
   }
 }
 
